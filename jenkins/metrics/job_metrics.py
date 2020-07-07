964f7476f9ca0c05a1e5dd6107b69e4d8bd6ec54
Nacho <icasado@stratio.com>
2020-07-07 09:56:22 +0200

    adding more info to last_build

--- jenkins/metrics/job_metrics.py
+++ jenkins/metrics/job_metrics.py
@@ -1,5 +1,7 @@
 from prometheus_client.core import GaugeMetricFamily
+import time
 
+current_milli_time = lambda: int(round(time.time() * 1000))
 
 def make_metrics(jobs):
 
@@ -93,4 +95,68 @@ def make_metrics(jobs):
 
     list_metrics.append(metric)
 
+    # Custom building time milliseconds
+    metric = None
+    metric = GaugeMetricFamily(
+        'jenkins_jobs_building_time_milliseconds_with_estimation',
+        'The running duration of a building job with its estimation',
+        labels=['job_name', 'status', 'number', 'estimatedDuration', 'displayName', 'fullDisplayName']
+    )
+
+    for job_id in list_jobs:
+        job = jobs.get_job(job_id)
+        last_build = job['last_build']
+        duration = int(last_build['duration'])
+        if duration > 0:
+            metric.add_metric(
+                labels=[job_id, last_build['result'], str(last_build['number']), str(last_build['estimatedDuration']), last_build["displayName"], last_build['fullDisplayName']],
+                value=duration
+            )
+        else:
+            if jobs.get_total_builds(job_id) == 0:
+                metric.add_metric(
+                    labels=[job_id, 'NOT_YET_BUILT', str(last_build['number']), str(last_build['estimatedDuration']), last_build["displayName"], last_build['fullDisplayName']],
+                    value=0
+                )
+            else:
+                metric.add_metric(
+                    labels=[job_id, 'BUILDING', str(last_build['number']), str(last_build['estimatedDuration']), last_build["displayName"], last_build['fullDisplayName']],
+                    value=current_milli_time() - last_build['timestamp']
+                )
+
+    list_metrics.append(metric)
+
+    # Custom building % of time upper estimated
+    metric = None
+    metric = GaugeMetricFamily(
+        'jenkins_jobs_building_percent_time_over_estimation',
+        'The percent time of the last building job over its estimation',
+        labels=['job_name', 'status', 'number', 'estimatedDuration', 'duration', 'displayName', 'fullDisplayName']
+    )
+
+    for job_id in list_jobs:
+        job = jobs.get_job(job_id)
+        last_build = job['last_build']
+        duration = int(last_build['duration'])
+        if duration > 0:
+            metric.add_metric(
+                labels=[job_id, last_build['result'], str(last_build['number']), str(last_build['estimatedDuration']),
+                        str(last_build['duration']), last_build["displayName"], last_build['fullDisplayName']],
+                value=(duration / last_build['estimatedDuration']) * 100
+            )
+        else:
+            if jobs.get_total_builds(job_id) == 0:
+                metric.add_metric(
+                    labels=[job_id, 'NOT_YET_BUILT', str(last_build['number']), str(last_build['estimatedDuration']),
+                            str(last_build['duration']), last_build["displayName"], last_build['fullDisplayName']],
+                    value=0
+                )
+            else:
+                metric.add_metric(
+                    labels=[job_id, 'BUILDING', str(last_build['number']), str(last_build['estimatedDuration']),
+                            str(last_build['duration']), last_build["displayName"], last_build['fullDisplayName']],
+                    value=(current_milli_time() - last_build['timestamp']) / last_build['estimatedDuration'] * 100
+                )
+
+    list_metrics.append(metric)
     return list_metrics
